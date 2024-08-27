f75329678e482e45b8bd98b88ba46ecb04a4e848
Gptharohalli <GPTHAROHALLI198@gmail.com>
2024-08-27 14:27:42 +0530

    Add files via upload

---
+++ amith.py
@@ -0,0 +1,6 @@
+#To write a paython program to convert decimal to binary, octal and hexadecimal
+dec = int(input("enter a decimal number:"))
+print("decimal value of",dec,"is")
+print(bin(dec),"in binary.")
+print(oct(dec),"in octal.")
+print(hex(dec),"in hexadecimal.")
